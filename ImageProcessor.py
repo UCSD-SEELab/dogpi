57666277354b2c018bf2ac824ff672979cd9cc6d
Marijose Obaya <A01280955@itesm.mx>
2019-08-08 15:34:17 -0700

    att shape

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -104,7 +104,7 @@ class ImageProcessor(Process):
 
 
         # vs = cv2.VideoCapture(args["video"])
-        vs = cv2.VideoCapture(1)
+        vs = cv2.VideoCapture(0)
         time.sleep(2.0)
 
         # keep looping
