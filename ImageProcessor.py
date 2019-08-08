978fdd3f897d07b33050843040c4cbe2be8a5c4f
Marijose Obaya <A01280955@itesm.mx>
2019-08-08 14:59:04 -0700

    frame shape

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -111,7 +111,6 @@ class ImageProcessor(Process):
         while True:
             # grab the current frame
             frame = vs.read()
-            frame = frame[1]
 
             half = ((frame.shape[1]) / 2)
             if frame is None:
